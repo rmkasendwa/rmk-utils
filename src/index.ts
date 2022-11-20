1bd10d0be96460c35d2fc90d8b9cba61dbc19959
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2022-11-21 01:27:58 +0300

    Refactors removeNullValues

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-export const removeNullValues = (object: Record<string, any>) => {
+/**
+ * Removes null/undefined values from an object.
+ *
+ * @param object The object with null values.
+ * @returns New object without null values.
+ */
+export const removeNullValues = <T extends Record<string, any>>(object: T) => {
   return Object.keys(object)
     .filter((key) => {
       return object[key] != null;
     })
     .reduce((accumulator, key) => {
-      accumulator[key] = object[key];
+      (accumulator as any)[key] = object[key];
       return accumulator;
-    }, {} as Record<string, any>);
+    }, {} as T);
 };
