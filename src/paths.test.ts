b490d702e969a1e6c45f387bdbb40b4bfd46ef45
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2023-03-09 11:02:30 +0300

    Adds test fro addToSearchParam function

--- src/paths.test.ts
+++ src/paths.test.ts
@@ -33,4 +33,24 @@ describe('addSearchParams', () => {
       '/users?sortBy[0][id]=1234&sortBy[0][sortDirection]=ASC&sortBy[1][id]=4587&sortBy[1][sortDirection]=DESC'
     );
   });
+  it('should be able to work work with both primitive types and object types in same url', () => {
+    expect(
+      addSearchParams(
+        '/users',
+        {
+          sortBy: [
+            { id: '1234', sortDirection: 'ASC' },
+            { id: '4587', sortDirection: 'DESC' },
+          ],
+          modifiedKeys: ['groupBy', 'sortBy'],
+          expandedGroupsInverted: true,
+        },
+        {
+          arrayParamStyle: 'append',
+        }
+      )
+    ).toBe(
+      '/users?sortBy[0][id]=1234&sortBy[0][sortDirection]=ASC&sortBy[1][id]=4587&sortBy[1][sortDirection]=DESC&modifiedKeys[0]=groupBy&modifiedKeys[1]=sortBy&expandedGroupsInverted=true'
+    );
+  });
 });
